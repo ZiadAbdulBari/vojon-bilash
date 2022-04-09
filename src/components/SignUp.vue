33c840fed09687104cf0500ab9392cd9d9db44e7
Ziad Abdul Bari <ziadabdulbari01@gmail.com>
2022-04-09 15:18:41 +0600

    project open

---
+++ src/components/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="signup-grid">
+                <div class="signup-form">
+                    <div class="logo">
+                        <img src="@/assets/images/logo.png" alt="">
+                    </div>
+                    <div>
+
+                        <div class="mb-3">
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Name">
+                        </div>
+                        <div class="mb-3">
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Email">
+                        </div>
+                        <div class="mb-3">
+                            <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Enter Password">
+                        </div>
+                        <div class="mb-3">
+                            <button class="btn">Signup</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="signup-img">
+                    <img src="@/assets/images/signup.avif" alt="">
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
