e3b856499f417ce8b17dae92fd7bb377c77c4a60
Ziad Abdul Bari <ziadabdulbari01@gmail.com>
2022-04-12 19:27:48 +0600

    show restaurant

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,13 +2,22 @@
     <div>
         <Header></Header>
         <div class="container">
-            <h1>Welcome to Home Page</h1>
+            <!-- <h1>Welcome to Home Page</h1> -->
+            <div class="card" style="width: 18rem;" v-for="(i) in restaurants" :key="i">
+                <!-- <img src="..." class="card-img-top" alt="..."> -->
+                <div class="card-body">
+                    <h5 class="card-title">{{i.name}}</h5>
+                    <p class="card-text">{{i.location}}</p>
+                    <a href="#" class="btn btn-primary">Detail</a>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import Header from '@/components/Header.vue'
+    import axios from 'axios';
     export default {
         name:'Home',
         components:{
@@ -16,7 +25,7 @@
         },
         data(){
             return{
-
+                restaurants:[],
             }
         },
         mounted(){
@@ -24,6 +33,18 @@
             if(!user){
                 this.$router.push({
                     name:'SignUp'
+                })
+            }
+        },
+        created(){
+            this.getRestaurantList();
+        },
+        methods:{
+            getRestaurantList(){
+                let url = 'http://localhost:3000/restaurants';
+                axios.get(url).then(response=>{
+                    console.log(response);
+                    this.restaurants = response.data;
                 })
             }
         }
