1708bf699afd6c01759faf5984bd132b690d1c4f
Ziad Abdul Bari <ziadabdulbari01@gmail.com>
2022-04-12 10:30:00 +0600

    header added

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img class="logo-img" src="@/assets/images/logo.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Add</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Update</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+       name:'Header', 
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
-        <h1>Welcome to Home Page</h1>
+        <Header></Header>
+        <div class="container">
+            <h1>Welcome to Home Page</h1>
+        </div>
     </div>
 </template>
 
 <script>
+    import Header from '@/components/Header.vue'
     export default {
         name:'Home',
+        components:{
+            Header
+        },
         data(){
             return{
 
