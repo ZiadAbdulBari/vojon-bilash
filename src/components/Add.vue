c7754603678e088fa5621b43891daab7685b6719
Ziad Abdul Bari <ziadabdulbari01@gmail.com>
2022-04-13 11:27:51 +0600

    add restaurent form done

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -2,7 +2,25 @@
     <div>
         <Header></Header>
         <div class="container">
-            <h1>Add</h1>
+            <div class="add-form">
+                <h1>Add Restaurant</h1>
+                <form>
+                    <div class="mb-3">
+                        <input type="email" v-model="name" placeholder="Restaurant Name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" v-model="address" placeholder="Location" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" v-model="phone" placeholder="Contact Number" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <!-- <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div> -->
+                    <button type="submit" @click="addRestaurant()" class="btn btn-primary">Add</button>
+                </form>
+            </div>
         </div>
     </div>
 </template>
@@ -13,7 +31,23 @@
         name:'Add',
         components:{
             Header
+        },
+        data(){
+            return{
+                name:'',
+                address:'',
+                phone:'',
+            }
+        },
+        // created(){
+        //     this.addRestaurant();
+        // },
+        methods:{
+            addRestaurant(){
+                console.log(this.name, this.address, this.phone);
+            }
         }
+        
     }
 </script>
 
