bcc4eda5f720719f56df53bccd83446d2bc240f2
Ziad Abdul Bari <ziadabdulbari01@gmail.com>
2022-04-14 08:22:31 +0600

    update done

--- src/components/Update.vue
+++ src/components/Update.vue
@@ -14,7 +14,7 @@
                     <div class="mb-3">
                         <input type="text" v-model="phone" placeholder="Contact Number" class="form-control" id="exampleInputPassword1">
                     </div>
-                    <button type="submit" @click="updateRestaurant()" class="btn btn-primary">Add</button>
+                    <button type="submit" @click="updateRestaurantData()" class="btn btn-primary">Update</button>
                 </form>
             </div>
         </div>
@@ -23,6 +23,7 @@
 
 <script>
     import Header from '@/components/Header.vue';
+import axios from 'axios';
     export default {
         name:'Update',
         components:{
@@ -33,15 +34,38 @@
                 name:'',
                 address:'',
                 phone:'',
-                id:this.$route.params
+                id:this.$route.params.id
             }
         },
         created(){
-            this.updateRestaurant();
+            this.getRestaurantData();
         },
         methods:{
-            updateRestaurant(){
-
+            getRestaurantData(){
+                let url = "http://localhost:3000/restaurants/"+this.id;
+                axios.get(url)
+                .then(response=>{
+                    console.log(response);
+                    this.name = response.data.name;
+                    this.address = response.data.location;
+                    this.phone = response.data.contact;
+                })
+            },
+            updateRestaurantData(){
+                let url = "http://localhost:3000/restaurants/"+this.id;
+                let data = {
+                    name:this.name,
+                    location:this.address,
+                    contact:this.phone
+                }
+                axios.put(url,data)
+                .then(response=>{
+                    if(response.status==200){
+                        this.$router.push({
+                            name:'Home'
+                        })
+                    }
+                })
             }
         }
     }
